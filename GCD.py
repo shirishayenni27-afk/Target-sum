ab17b10ec266e4685f0c7515f36293df15114a56
shirisha yenni <shirishayenni27@gmail.com>
2026-06-05 07:33:07 +0530

    Remove GCD calculation code from GCD.py
    Remove the implementation of GCD calculation.

--- GCD.py
+++ GCD.py
@@ -1,12 +1,1 @@
-# GCD OF TWO NUMBERS 
-
-x=input().split() #sperate & store the value
-a=int(x[0]) #first element in list
-b=int(x[1]) #second element in list
-small=min(a,b)
-gcd=1
-for i in range(1,small+1):
-    if a%i==0 and b%i==0:
-        gcd=i
-print(gcd)
 
